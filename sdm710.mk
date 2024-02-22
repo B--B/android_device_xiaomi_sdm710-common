2b3f13a84d7acc817cb13dd7f2265538a222c19e
Marco Zanin <mrczn.bb@gmail.com>
2024-02-22 14:17:51 +0100

    sdm710-common: VNDK: ship libs in a better way
    And remove prebuilt VNDK libs from vendor repo
    
    Signed-off-by: Marco Zanin <mrczn.bb@gmail.com>

--- sdm710.mk
+++ sdm710.mk
@@ -442,8 +442,9 @@ PRODUCT_PACKAGES += \
     vendor.qti.hardware.vibrator.service
 
 # VNDK
-PRODUCT_COPY_FILES += \
-    prebuilts/vndk/v33/arm64/arch-arm64-armv8-a/shared/vndk-sp/libutils.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libutils-v33.so
+PRODUCT_PACKAGES += \
+    libstagefright_foundation-v33 \
+    libutils-v33
 
 # Wi-Fi
 PRODUCT_PACKAGES += \
