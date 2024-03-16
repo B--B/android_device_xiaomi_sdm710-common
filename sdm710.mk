22fe543cb563a4515aaf061e8165ec88bbb035c0
Marco Zanin <mrczn.bb@gmail.com>
2024-03-16 03:27:55 +0100

    sdm710-common: import media profiles from MIUI
    Profiles imported from MIUI V12.5.4.0.RFBCNXM
    
    - Removed not supported profiles
    - Dropped OMX.qcom.video.decoder.vp9
    - Updated for A14
    - Removed media configs from proprietary files list
    - Removed duplicated libavservices_minijail.vendor package
    
    Signed-off-by: Marco Zanin <mrczn.bb@gmail.com>

--- sdm710.mk
+++ sdm710.mk
@@ -248,8 +248,7 @@ PRODUCT_PACKAGES += \
 # Media
 PRODUCT_PACKAGES += \
     libavservices_minijail \
-    libavservices_minijail.vendor \
-    libavservices_minijail_vendor
+    libavservices_minijail.vendor
 
 # Media configs
 PRODUCT_COPY_FILES += \
@@ -260,6 +259,13 @@ PRODUCT_COPY_FILES += \
     frameworks/av/media/libstagefright/data/media_codecs_google_telephony.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_telephony.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_video.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_video_le.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_video_le.xml
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/media/media_codecs.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs.xml \
+    $(LOCAL_PATH)/configs/media/media_codecs_performance.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_performance.xml \
+    $(LOCAL_PATH)/configs/media/media_profiles.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_profiles.xml \
+    $(LOCAL_PATH)/configs/media/media_profiles.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_profiles_V1_0.xml \
+    $(LOCAL_PATH)/configs/media/media_profiles_vendor.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_profiles_vendor.xml
 
 # Mlipay
 PRODUCT_PACKAGES += \
